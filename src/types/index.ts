28d8f1302757a05a09a21657a0154a7d8902b749
DreamHarakiri <derek.gaming744@gmail.com>
2024-05-11 15:00:52 +0300

    боже помоги

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,58 @@
-interface ICard {
+export type TProductCategory =
+	| 'софт-скилл'
+	| 'другое'
+	| 'дополнительное'
+	| 'кнопка'
+	| 'хард-скил';
+export type TPayment = 'онлайн' | 'при получении' | '';
+
+export interface IProduct {
 	id: string;
-	description: string;
-	image: string;
+	category: TProductCategory;
 	title: string;
-	category: string;
+	description: string;
 	price: number;
+	image?: string;
+	selected: boolean;
+	index?: number;
 }
 
-interface IOrder {
-	email: string;
-	phone: string;
-	address: string;
-	payment: string;
-	total: number;
-	items: string[];
+export interface IPage {
+	catalog: HTMLElement[];
+	counter: number;
+	locked: boolean;
 }
+
+export interface IOrderContact {
+	email?: string;
+	phone?: string;
+}
+
+export interface IOrderDelivery {
+	payment?: TPayment;
+	address?: string;
+}
+
+export interface IOrder extends IOrderContact, IOrderDelivery {
+	items: string[];
+	total: number | string;
+}
+
+export interface IOrderSuccess {
+	id: string;
+	total: number;
+}
+
+export interface IAppState {
+	catalog: IProduct[];
+	basket: IProduct[] | [];
+	preview: string | null;
+	order: IOrder | null;
+	loading: boolean;
+}
+
+export interface ICardActions {
+	onClick: (event: MouseEvent) => void;
+}
+
+export type FormError = Partial<Record<keyof IOrder, string>>;
